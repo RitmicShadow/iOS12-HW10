8a3142cad5def19bc303306b8e9cb1390319a556
DenisMayshev <denmay13@mail.ru>
2024-01-15 17:54:04 +0400

    delete all comments

--- iOS12-HW10/Sources/ViewController.swift
+++ iOS12-HW10/Sources/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  iOS12-HW10
-//
-//  Created by Ден Майшев on 15.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
